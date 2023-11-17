4c2f3f9b3665462633a2be22bfb1293ebc3a16bb
Peaky00 <chris.kirkley@yahoo.com>
2023-11-17 03:44:50 -0600

    got trash can working

--- public/notes.js
+++ public/notes.js
@@ -4,20 +4,57 @@ const noteText = document.getElementById('noteText');
 const noteList = document.getElementById('noteList');
 
 // Function to fetch and display existing notes
+// Function to fetch and display existing notes
 async function displayNotes() {
-  const response = await fetch('/api/notes');
-  const notes = await response.json();
-
-  // Clear the note list
-  noteList.innerHTML = '';
-
-  // Display each note
-  notes.forEach((note) => {
-    const noteItem = document.createElement('div');
-    noteItem.textContent = note.text;
-    noteList.appendChild(noteItem);
-  });
-}
+    const response = await fetch('/api/notes');
+    const notes = await response.json();
+  
+    // Clear the note list
+    noteList.innerHTML = '';
+  
+    // Display each note with a trash can icon
+    notes.forEach((note) => {
+        const noteItem = document.createElement('div');
+        noteItem.classList.add('note-item');
+        noteItem.innerHTML = `
+          <div>${note.text}</div>
+          <button class="delete-note" data-note-id="${note.id}">
+            <i class="fas fa-trash"></i> <!-- FontAwesome trash can icon -->
+          </button>
+        `;
+      
+        // Add a click event listener to the trash can icon button
+        const deleteButton = noteItem.querySelector('.delete-note');
+        deleteButton.addEventListener('click', deleteNote);
+      
+        noteList.appendChild(noteItem);
+      });
+      
+  }
+  
+  // Function to handle deleting a note
+  async function deleteNote(e) {
+    const noteId = e.target.dataset.noteId;
+    console.log('Note ID to delete:', noteId); 
+  
+    // Send a DELETE request to delete the note by ID
+    const response = await fetch(`/api/notes/${noteId}`, {
+      method: 'DELETE',
+    });
+  
+    if (response.ok) {
+      // Refresh the displayed notes after deleting
+      displayNotes();
+    } else {
+      console.error('Failed to delete note.');
+    }
+  }
+  
+  // ... Other code for form submission, clearing form, and initial display
+  
+  // Initial display of existing notes
+  displayNotes();
+  
 
 // Event listener for form submission
 noteForm.addEventListener('submit', async (e) => {
--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const notesRoutes = require('./routes/notesRoutes'); // Import your routes module
+
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-// Route to serve the notes.html file
-app.get('/notes', (req, res) => {
-    // Return the notes.html file
-    res.sendFile(__dirname + '/public/notes.html');
-  });
-  
 
 // Middleware to parse JSON data
 app.use(express.json());
@@ -16,8 +13,95 @@ app.use(express.urlencoded({ extended: true }));
 // Serve static files from the "public" directory
 app.use(express.static('public'));
 
-// Import and use the notesRoutes module
-const notesRoutes = require('./routes/notesRoutes');
+// Define API routes (You can also put these in a separate router file)
+app.get('/api/notes', (req, res) => {
+  // Read the db.json file and return all saved notes as JSON
+  fs.readFile(path.join(__dirname, 'db.json'), 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Internal Server Error' });
+      return;
+    }
+    const notes = JSON.parse(data);
+    res.json(notes);
+  });
+});
+
+app.post('/api/notes', (req, res) => {
+  // Receive a new note in the request body
+  const newNote = req.body;
+
+  // Read the existing notes from db.json
+  fs.readFile(path.join(__dirname, 'db.json'), 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Internal Server Error' });
+      return;
+    }
+    const notes = JSON.parse(data);
+
+    // Generate a unique ID based on the current timestamp
+    newNote.id = Date.now().toString();
+
+    // Add the new note to the array of notes
+    notes.push(newNote);
+
+    // Write the updated notes array back to db.json
+    fs.writeFile(path.join(__dirname, 'db.json'), JSON.stringify(notes), (err) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal Server Error' });
+        return;
+      }
+      res.json(newNote);
+    });
+  });
+});
+app.delete('/api/notes/:id', (req, res) => {
+    const noteId = req.params.id;
+  
+    // Read the existing notes from db.json
+    fs.readFile(path.join(__dirname, 'db.json'), 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal Server Error' });
+        return;
+      }
+      const notes = JSON.parse(data);
+  
+      // Find the index of the note with the given ID
+      const noteIndex = notes.findIndex((note) => note.id === noteId);
+  
+      if (noteIndex === -1) {
+        // Note not found
+        res.status(404).json({ error: 'Note not found' });
+        return;
+      }
+  
+      // Remove the note from the array
+      notes.splice(noteIndex, 1);
+  
+      // Write the updated notes array back to db.json
+      fs.writeFile(path.join(__dirname, 'db.json'), JSON.stringify(notes), (err) => {
+        if (err) {
+          console.error(err);
+          res.status(500).json({ error: 'Internal Server Error' });
+          return;
+        }
+        res.json({ message: 'Note deleted successfully' });
+      });
+    });
+  });
+// Define routes for your HTML pages
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Define routes using the notesRoutes module (if you have one)
 app.use('/', notesRoutes);
 
 // Start the server
